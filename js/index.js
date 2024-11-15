5c3281b5d5f038d4b70721aad5d1a8c5c64579e8
Ramsey <ramseyediku@protonmail.com>
2024-11-15 04:06:05 +0000

    final stylings and bug fixes, plus currency switch implementation

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     let currencyDropdowns = document.querySelectorAll("select")
-    let container = document.querySelector(".container")
-    let currencyInputs = document.querySelectorAll("input[type='number']")
+    let switcher = document.querySelector(".switch")
+    let amount = document.querySelector("input[name='amount']")
     let form = document.querySelector("form");
+    let results = document.querySelector(".results");
     let defaultCurrency = "USD";
 
-    // fetch all possible usable currencies from API
+    // fetch all possible usable currencies from API so as to populated the field onl;y with valid currencies
     fetch(`https://v6.exchangerate-api.com/v6/5f4ac2b2db62a913579d2dbc/latest/${defaultCurrency}`, {
         method: 'GET',
     })
@@ -20,17 +21,16 @@ document.addEventListener("DOMContentLoaded", () => {
      .catch(error => console.log(error))
 
     
-
+    // on form submit fetch data from the API based on currency selected (to and from) and amount
     form.addEventListener("submit", e => {
         e.preventDefault();
 
-        let results = document.querySelector(".results")
-        if (results != null) results.remove()
+        // wipe out screen of previous result
+        results.innerHTML = "";
 
         let currencyFrom = currencyDropdowns[0].value
         let currencyTo = currencyDropdowns[1].value
 
-        let amount = document.querySelector("input[name='amount']")
 
         fetch(`https://v6.exchangerate-api.com/v6/5f4ac2b2db62a913579d2dbc/pair/${currencyFrom}/${currencyTo}/${amount.value}`, {
             method: 'GET',
@@ -42,10 +42,17 @@ document.addEventListener("DOMContentLoaded", () => {
                     console.log("Initial currency fetch failed")
                 }
             }
-        ).then(data => showConversionResult(data.conversion_rate, data.conversion_result))
+        ).then(data => showConversionResult(amount.value,data.conversion_rate, data.conversion_result, data.base_code, data.target_code, data.time_last_update_utc))
          .catch(error => console.log(error))
     
 
+    })
+
+
+    switcher.addEventListener("click", ()=> {
+        let temp = currencyDropdowns[0].value 
+        currencyDropdowns[0].value = currencyDropdowns[1].value
+        currencyDropdowns[1].value = temp
     })
 
     // populate dropdown fields based on the current currencies the API supports
@@ -63,13 +70,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     // show currency conversion results 
-    function showConversionResult(rate,result) {
-        currencyInputs[1].value = result
+    function showConversionResult(amount,rate,result, baseCode, targetCode,timestamp) {
 
-        let results = document.createElement("div")
-        results.className = "results"
-        results.innerText = rate;
-        container.append(results)
+        let date =  new Date(timestamp)
+        let month = date.toLocaleString('default', { month: 'short' });
+
+        // let time = `${date.getDay()} ${month} ${date.getFullYear()}, ${date.getHours()} ${date.getMinutes()} UTC+1.0`
+
+        results.innerHTML = `
+        <h3>These are the latest conversion results</h3><br>
+        <div class="results__base">${amount} ${baseCode} =</div> 
+        <div class="results__target">${result} ${targetCode}</div><br>
+        <small>The conversion rate is: 1 ${baseCode} = ${rate} ${targetCode}.<br><br> Ratings last updated: <br>${timestamp}</small>
+        `;
 
     }
 });
